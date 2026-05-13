465eba8459964db13f48cf5c2a58968aad13f427
Muhamad Irsal <muhamadirsal@gmail.com>
2026-05-13 13:47:19 +0700

    feat: init repo

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="bg-white rounded-2xl shadow-lg p-8 w-full max-w-md"></div>
+    </div>
+  );
+}
